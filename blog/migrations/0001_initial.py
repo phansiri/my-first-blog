3c874eb093dee29cff7f1d8eec601e4fb128d4e3
Lit Phansiri <phansiri@uw.edu>
2015-07-30 00:44:08 +0000

    My Mini_Lab4 Django Girls app, first commit

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('create_date', models.DateTimeField(default=datetime.datetime(2015, 7, 30, 0, 19, 9, 395293, tzinfo=utc))),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
